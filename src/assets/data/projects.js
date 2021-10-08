08540500e2f3bd68496186c82dc139ddf5bc1c17
Diegocorp <diegosandovalr.corp@gmail.com>
2021-10-08 15:08:47 -0700

    charlesgranded project

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,29 +1,38 @@
 import { v4 as uuidv4 } from "uuid";
-import CarfenSoftImg from "../images/carfensoft.png";
-import MasterNETImg from "../images/masternet.png";
-import JoaquinJaureguiImg from "../images/joaquinjauregui.png";
+import CarfenSoftImg from "../images/projects/carfensoft.png";
+import MasterNETImg from "../images/projects/masternet.png";
+import JoaquinJaureguiImg from "../images/projects/joaquinjauregui.png";
+import CharlesGrandedImg from "../images/projects/charlesgranded.png";
 
 const projects = [
   {
     id: uuidv4(),
     name: "MasterNET Website",
-    desc: "Website developed to promote the startup MasterNET in France using React.js technology.",
+    desc: "Website developed to promote the startup MasterNET in France. Made with React.js technology.",
     img: MasterNETImg,
     link: "https://www.masternet.ga/",
   },
   {
     id: uuidv4(),
-    name: "CarfenSoft Website",
-    desc: "Website developed to promote the software development company CarfenSoft using React.js technology.",
-    img: CarfenSoftImg,
-    link: "https://www.carfensoft.ga/",
+    name: "Joaquin Jauregui Portfolio",
+    desc: "Personal portfolio developed for the lawyer Joaquin Jauregui market for promotion in the american market. Made with React.js technology.",
+    img: JoaquinJaureguiImg,
+    link: "https://www.joaquinjauregui.ml/",
   },
   {
     id: uuidv4(),
-    name: "Joaquin Jauregui Portfolio",
-    desc: "Personal portfolio developed for the lawyer Joaquin Jauregui market for promotion in the american market using React.js technology.",
-    img: JoaquinJaureguiImg,
-    link: "https://www.joaquinjauregui.ml/",
+    name: "CarfenSoft Website",
+    desc: "Website developed to promote the software development company CarfenSoft. Made with React.js technology.",
+    img: CarfenSoftImg,
+    link: "https://www.carfensoft.ga/",
+  },
+
+  {
+    id: uuidv4(),
+    name: "Charles Granded Portfolio",
+    desc: "Personal portfolio developed for Charles Granded freelance programmer specialized in Back-End. Made with React.js technology.",
+    img: CharlesGrandedImg,
+    link: "https://www.charlesgranded.cf/",
   },
 ];
 
