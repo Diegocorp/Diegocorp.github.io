637d69cf1e912c2be6a0cfb87e3a9473ec373094
Diegocorp <diegosandovalr.corp@gmail.com>
2021-10-24 12:12:17 -0700

    try with roots

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import GlobalStyles from './styles/GlobalStyle';
-import Typography from './styles/Typography';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import About from "./pages/About";
+import About from "./pages/Contact";
+import About from "./pages/Projects";
+import GlobalStyles from "./styles/GlobalStyle";
+import Typography from "./styles/Typography";
 
 ReactDOM.render(
   <>
     <GlobalStyles />
     <Typography />
     <App />
+    <About />
+    <Contact />
+    <Projects />
   </>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
