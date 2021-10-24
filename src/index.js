937131f222561ff6ecd19ea980ed580ab212d102
Diegocorp <diegosandovalr.corp@gmail.com>
2021-10-24 12:19:44 -0700

    roots deleted

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
 import GlobalStyles from "./styles/GlobalStyle";
 import Typography from "./styles/Typography";
 
@@ -12,9 +9,6 @@ ReactDOM.render(
     <GlobalStyles />
     <Typography />
     <App />
-    <About />
-    <Contact />
-    <Projects />
   </>,
   document.getElementById("root")
 );
